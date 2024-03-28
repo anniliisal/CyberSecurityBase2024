b9262f1c75930ab988f8a1a95f525e93608da7d3
anniliisal <76066697+anniliisal@users.noreply.github.com>
2024-03-28 04:45:08 +0200

    Create backend.py

---
+++ poll_project/backend.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+#Flaw 3:
+class NoHashModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        User = get_user_model()
+        try:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
