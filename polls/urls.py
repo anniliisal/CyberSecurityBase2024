881ea222e0064eef2545a4f4b5b26c8da55040e1
anniliisal <76066697+anniliisal@users.noreply.github.com>
2024-03-28 04:46:50 +0200

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.urls import include
 app_name = 'polls'
 
 urlpatterns = [
@@ -25,4 +25,6 @@ urlpatterns = [
     #  URLs for registration
     path('register/', views.register, name='register'),  
     path('registrationsuccess/', views.registrationsuccess, name='registrationsuccess'),  
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
