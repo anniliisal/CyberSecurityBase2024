80203ba23c4f6396d27e666d2964e97998b9c11d
anniliisal <76066697+anniliisal@users.noreply.github.com>
2024-03-28 04:45:53 +0200

    Update settings.py

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#Flaw 3/fix: this should be removed, since this disables password hashing
+AUTHENTICATION_BACKEND = [
+    'polls.backend.NoHashModelBackend',
+    'django.contrib.auth.backend.ModelBackend',
+]
